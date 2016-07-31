26a6ffa054c0d8a8737f24e132ba46caf6c26fe1
renzon <renzon@gmail.com>
2016-07-31 12:04:26 -0300

    Mudando para flask

--- appengine/main.py
+++ appengine/main.py
@@ -1,30 +1,27 @@
-#!/usr/bin/env python
-# coding: utf8
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KINs, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+"""`main` is the top level module for your Flask application."""
+from __future__ import absolute_import, unicode_literals
 
-import webapp2
+# Import the Flask Framework
+from flask import Flask
+app = Flask(__name__)
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
 
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world 2!')
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World Flask!'
 
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
