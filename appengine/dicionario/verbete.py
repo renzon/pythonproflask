f109721e4c1abfe7bf724d7e172d416ddfae8232
renzon <renzon@gmail.com>
2016-08-07 11:48:21 -0300

    Delete ok

--- appengine/dicionario/verbete.py
+++ appengine/dicionario/verbete.py
@@ -3,8 +3,12 @@
 from __future__ import absolute_import, unicode_literals
 
 # Import the Flask Framework
+
+from google.appengine.ext import ndb
+
 from dicionario.modelo import Verbete
 from flask import Flask, request, jsonify
+from flask.json import jsonify
 
 app = Flask(__name__)
 
@@ -42,6 +46,14 @@ def editar(id):
     verbete.descricao = descricao
     verbete.put()
     return jsonify(verbete_dct(verbete))
+
+
+@app.route('/verbete/apagar/<int:id>')
+def apagar(id):
+    """Salva um verbete em BD"""
+    key = ndb.Key(Verbete, id)
+    key.delete()
+    return jsonify('ok')
 
 
 def verbete_dct(verbete):
