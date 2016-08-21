eec029ae06ea3b7379b501d17c00d6a8c7ed1277
renzon <renzon@gmail.com>
2016-08-21 09:56:09 -0300

    Edição ok

--- appengine/dicionario/pages.py
+++ appengine/dicionario/pages.py
@@ -16,22 +16,34 @@ def listar():
     query = Verbete.query().order(-Verbete.palavra).order(Verbete.criacao)
     verbetes = query.fetch()
     for v in verbetes:
-        v.delete_path = url_for('verbete.apagar', id=v.key.id())
+        key_id = v.key.id()
+        v.delete_path = url_for('verbete.apagar', id=key_id)
+        v.edit_path = url_for('verbete.form', id=key_id)
+    return render_template(
+        'listar.html',
+        verbetes=verbetes,
+        novo_url=url_for('verbete.form')
+    )
 
-    return render_template('listar.html', verbetes=verbetes)
+
+@verbete.route('/editar/<int:id>', methods=['POST'])
+def editar(id):
+    """Edita um verbete em BD"""
+    palavra = request.form.get('palavra')
+    descricao = request.form.get('descricao')
+    verbete = Verbete.get_by_id(id)
+    verbete.populate(palavra=palavra, descricao=descricao)
+    verbete.put()
+    return redirect(url_for('verbete.listar'))
 
 
-@verbete.route('/form')
-def form():
-    """Salva um verbete em BD"""
-    # palavra = request.args['palavra']
-    # descricao = request.args.get('descricao')
-    # verbete = Verbete(palavra=palavra, descricao=descricao)
-    # verbete.put()
-    query = Verbete.query().order(-Verbete.palavra).order(Verbete.criacao)
-    verbetes = query.fetch()
-
-    return render_template('form.html', verbetes=verbetes)
+@verbete.route('/form/<int:id>')
+@verbete.route('/form', defaults={'id': None})
+def form(id):
+    """Mostra form de verbete"""
+    verbete = None if id is None else Verbete.get_by_id(id)
+    form_url = url_for('verbete.save') if id is None else url_for('verbete.editar', id=id)
+    return render_template('form.html', verbete=verbete, form_url=form_url)
 
 
 @verbete.route('/salvar', methods=['POST'])
@@ -42,18 +54,6 @@ def save():
     verbete = Verbete(palavra=palavra, descricao=descricao)
     verbete.put()
     return redirect(url_for('verbete.listar'))
-
-
-@verbete.route('/editar/<int:id>')
-def editar(id):
-    """Salva um verbete em BD"""
-    palavra = request.args['palavra']
-    descricao = request.args.get('descricao')
-    verbete = Verbete.get_by_id(id)
-    verbete.palavra = palavra
-    verbete.descricao = descricao
-    verbete.put()
-    return jsonify(verbete_dct(verbete))
 
 
 @verbete.route('/apagar/<int:id>', methods=['POST'])
